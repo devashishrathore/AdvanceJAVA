24762fced055de07b55f779b604c3329fe92eb62
nikhilrathod1 <93572041+devashishrathore@users.noreply.github.com>
2025-03-17 01:30:33 +0530

    Done

--- AdvanceJava/src/dataStructure/MapClass.java
+++ AdvanceJava/src/dataStructure/MapClass.java
@@ -2,6 +2,6 @@ package dataStructure;
 
 public class MapClass {
 	public static void main(String[] args) {
-		System.out.println("MAP Interface check for asking creds");
+		System.out.println("MAP Interface");
 	}
 }
