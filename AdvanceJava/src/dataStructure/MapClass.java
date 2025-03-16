1f1dca9794ac48930bd39e3eefde3d2b744c9812
nikhilrathod1 <93572041+devashishrathore@users.noreply.github.com>
2025-03-17 01:17:55 +0530

    creds save for eclipse Everytime

--- AdvanceJava/src/dataStructure/MapClass.java
+++ AdvanceJava/src/dataStructure/MapClass.java
@@ -2,6 +2,6 @@ package dataStructure;
 
 public class MapClass {
 	public static void main(String[] args) {
-		System.out.println("MAP Interface");
+		System.out.println("MAP Interface now Done Creds Save");
 	}
 }
